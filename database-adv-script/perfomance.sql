aa3e5159efe50f555b42edbd25afbc407469422c
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-12-01 14:30:54 +0300

    [feat]: refactor complex queries to improve performance

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,28 @@
+EXPLAIN ANALYZE
+SELECT
+    Booking.booking_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    Booking.status,
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    User.email,
+    Property.property_id,
+    Property.name AS property_name,
+    Property.location,
+    Property.pricepernight,
+    Payment.payment_id,
+    Payment.amount,
+    Payment.amount,
+    Payment.payment_date,
+    Payment.payment_method
+FROM
+    Booking
+JOIN
+    User ON Booking.user_id = User.user_id
+JOIN
+    Property ON Booking.property_id = Property.property_id
+LEFT JOIN
+    Payment ON Booking.booking_id = Payment.booking_id;
